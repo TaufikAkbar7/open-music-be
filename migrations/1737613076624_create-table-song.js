c89e777aaf334496c8cb7098c0a143e387cd35d5
TaufikAkbar7 <taufik.lol911@gmail.com>
2025-01-23 13:20:59 +0700

    separate migration table album and song

---
+++ migrations/1737613076624_create-table-song.js
@@ -0,0 +1,34 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+ pgm.createTable('t_song', {
+   id: {
+    type: 'uuid',
+    primaryKey: true
+   },
+   album_id: {
+    type: 'uuid',
+    notNull: false,
+    references: '"t_album"',
+    onDelete: 'cascade'
+   },
+   title: { type: 'varchar(255)', notNull: true },
+   genre: { type: 'varchar(255)', notNull: true },
+   performer: { type: 'varchar(255)', notNull: true },
+   duration: { type: 'int4', notNull: false },
+   year: { type: 'int4', notNull: true },
+ });
+ pgm.createIndex('t_song', 'album_id');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+ pgm.dropTable('t_song');
+};
